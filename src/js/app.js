d98c4873de3e9ba89fa6abb2c0a5c6e8cbad9781
TiagoGomides <gomidestiago@outlook.com>
2025-04-07 02:40:29 -0300

    :sparkles: feat(carrossel): Criar arquivo JS para inicialização do Swiper
    Criação do script dedicado para configurar e inicializar o carrossel com Swiper.js, incluindo parâmetros como loop, navegação, paginação e velocidade.

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".swiper", {
+    loop:true,
+    grabCursor:true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+      },
+      breakpoints: {
+       
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 18
+        },
+        
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 18
+        },
+        1188: {
+          slidesPerView: 5,
+          spaceBetween: 24
+        }
+      }
+    
+  });
