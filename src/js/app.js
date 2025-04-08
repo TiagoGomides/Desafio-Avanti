616047577449a82446e5c416e32a10ab8ebbe745
TiagoGomides <gomidestiago@outlook.com>
2025-04-08 15:35:25 -0300

    :sparkles: feat(pesquisa): Implementar lógica de busca em JavaScript
    Criação da funcionalidade de pesquisa utilizando JavaScript, com captura de input do usuário.

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,60 @@
 var swiper = new Swiper(".swiper", {
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
   autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".custom-next",
     prevEl: ".custom-prev",
   },
   pagination: {
-      el: ".swiper-pagination",
-      clickable:true,
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 18,
     },
-    breakpoints: {
-     
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 18
-      },
-      
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 18
-      },
-      1188: {
-        slidesPerView: 5,
-        spaceBetween: 24
-      }
-    }
-  
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1188: {
+      slidesPerView: 5,
+      spaceBetween: 24,
+    },
+  },
+});
+
+const campo = document.getElementById("campoBusca");
+const resultado = document.getElementById("resultadoBusca");
+
+let timeoutId;
+
+function fazerBusca() {
+  const valorBusca = campo.value.trim();
+
+  clearTimeout(timeoutId); 
+
+  if (valorBusca) {
+    resultado.textContent = `Você buscou por: '${valorBusca}'`;
+  } else {
+    resultado.textContent = "Por favor, digite algo para buscar.";
+
+    
+    timeoutId = setTimeout(() => {
+      resultado.textContent = "";
+    }, 4000);
+  }
+}
+
+campo.addEventListener("input", () => {
+  if (campo.value.trim() === "") {
+    resultado.textContent = "";
+    clearTimeout(timeoutId);
+  }
 });
